9d08805d1e89adb918e6b98fc7f67700a7b15d61
Randy Steele <steelerx155@gmail.com>
2019-10-20 02:39:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,13 +36,13 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder"] = 4              
+                
   # add 2 muskets to the shipping_manifest hash below
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
